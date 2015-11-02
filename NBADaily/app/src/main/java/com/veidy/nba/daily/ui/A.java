188c4ceb1173f0df9474a9a3f34e6620ae755d7c
veidy <weilong.guo@916816.com>
2015-11-02 14:40:55 +0800

    baseactivity

---
+++ NBADaily/app/src/main/java/com/veidy/nba/daily/ui/A.java
@@ -0,0 +1,12 @@
+package com.veidy.nba.daily.ui;
+
+/**
+ * @author : veidy
+ * @version : 1.0
+ * @Description : NBADaily
+ * @Copyright : GL. All Rights Reserved
+ * @Company : 江苏国立网络科技有限公司
+ * @Create Date  : 2015/11/2  14:32
+ */
+public class A {
+}
---
+++ NBADaily/app/src/main/java/com/veidy/nba/daily/ui/base/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.veidy.nba.daily.ui.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author : veidy
+ * @version : 1.0
+ * @Description : NBADaily
+ * @Create Date  : 2015/11/2  14:39
+ */
+public class BaseActivity  extends AppCompatActivity{
+}
